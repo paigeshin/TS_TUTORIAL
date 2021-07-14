44cfbd843006c7dd0e2b1997a77749bafb44104f
innertainment <innertainment@innertainmentui-MacBookPro.local>
2021-07-14 10:04:39 +0900

    merge

--- mini_project/dist/app.js
+++ mini_project/dist/app.js
@@ -1,15 +1,18 @@
 "use strict";
 class ProjectInput {
-    constructor() {
-        this.templateElement = document.getElementById("project-input");
-        this.hostElement = document.getElementById("app");
-        const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild;
-        this.attach();
-    }
-    attach() {
-        this.hostElement.insertAdjacentElement("afterbegin", this.element);
-    }
+  constructor() {
+    this.templateElement = document.getElementById("project-input");
+    this.hostElement = document.getElementById("app");
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild;
+    this.attach();
+  }
+  attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
 }
 const prjinput = new ProjectInput();
 //# sourceMappingURL=app.js.map
